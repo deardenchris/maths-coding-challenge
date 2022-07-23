2a9c55be040a7017edfbf43d34ff957b20cb5476
Chris Dearden <chris.dearden@stfc.ac.uk>
2022-07-23 14:53:25 +0100

    Initial code template

---
+++ calc.py
@@ -0,0 +1,22 @@
+
+# Maths coding challenge!
+
+# 1) Ask user to enter an integer and store as a variable
+#    Bonus points for checking that the input is an integer and raise an exception if not!
+
+# 2) Write functions to calculate the square, cube, square root and reciprocal for the given integer
+
+# For example: 
+
+def calc_square(value):
+   return value * value
+   
+# 3) Call each function by passing the integer variable to each 
+
+# For example:
+
+square = calc_square(value)
+
+# 4) print the results to screen
+
+print('Square of', value, 'is :', square)
